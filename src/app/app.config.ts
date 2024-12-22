fa98d4920df85d6c3e771f39f6533d2d2b18a978
jonsant <jonssonanton@hotmail.com>
2024-12-22 14:40:35 +0100

    basic angular pwa working

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, isDevMode } from '@angular/core';
 import { provideRouter, RouteReuseStrategy } from '@angular/router';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatListModule } from '@angular/material/list';
@@ -12,17 +12,20 @@ import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { AuthInterceptor } from './interceptors/AuthInterceptor';
 import { CacheRouteReuseStrategy } from './CustomRouteReuseStrategy';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     importProvidersFrom(BrowserModule, MatButtonModule, MatToolbarModule, MatListModule, MatMenuModule),
     importProvidersFrom(BrowserModule),
-    provideHttpClient(
-      withInterceptors([AuthInterceptor])
-    ),
+    provideHttpClient(withInterceptors([AuthInterceptor])),
     provideAnimations(),
     provideRouter(routes),
     { provide: LocationStrategy, useClass: HashLocationStrategy },
-    { provide: RouteReuseStrategy, useClass: CacheRouteReuseStrategy }
+    { provide: RouteReuseStrategy, useClass: CacheRouteReuseStrategy },
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: true,
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ]
 };
